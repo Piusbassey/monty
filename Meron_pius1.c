001508fd2998d5475a0bbd3738537d7884079b44
meron6 <khozameron5@gmail.com>
2024-01-17 08:40:36 -0800

    Meron_pius1.c

--- Meron_pius1.c
+++ Meron_pius1.c
@@ -1,53 +1,26 @@
 #include "monty.h"
 
 /**
- * push - Pushes an element to the stack.
- * @stack: A pointer to the top of the stack.
- * @line_number: The line number of the opcode.
- * @n: The integer to be pushed onto the stack.
+ *  Meron_pius1- print all values on `stack' starting from the top
+ * @stack: double pointer to head of stack
+ * @line_number: line number being executed from script file
+ *
+ * Return: void
  */
-void push(stack_t **stack, unsigned int line_number, int n)
+void Meron_pius1(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node;
+	stack_t *head;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+	(void)(line_number);
+
+	head = *stack;
+	while (head != NULL)
 	{
-		fprintf(stderr, "Error: malloc failed\n");
-		free_stack(*stack);
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = n;
-	new_node->prev = NULL;
-
-	if (*stack == NULL)
-	{
-		new_node->next = NULL;
-		*stack = new_node;
-	}
-	else
-	{
-		new_node->next = *stack;
-		(*stack)->prev = new_node;
-		*stack = new_node;
-	}
-}
-
-/**
- * pall - Prints all the values on the stack.
- * @stack: A pointer to the top of the stack.
- * @line_number: The line number of the opcode.
- */
-void pall(stack_t **stack, unsigned int line_number)
-{
-	stack_t *temp = *stack;
-
-	(void)line_number;
-
-	while (temp != NULL)
-	{
-		printf("%d\n", temp->n);
-		temp = temp->next;
+		printf("%d\n", head->n);
+		head = head->next;
+		if (head == *stack)
+		{
+			return;
+		}
 	}
 }
