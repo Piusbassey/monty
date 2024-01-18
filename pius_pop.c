e3c552fb26625d21162e08d41ff25627adeefede
meron6 <khozameron5@gmail.com>
2024-01-18 07:09:16 -0800

    pius_pop.c

--- pius_pop.c
+++ pius_pop.c
@@ -1,23 +1,29 @@
 #include "monty.h"
 
 /**
- * pius_ pop - Removes the top element of the stack.
- * @stack: A pointer to the top of the stack.
- * @line_number: The line number of the opcode.
+ * pius_pop - pop top element off of `stack'
+ * @stack: double pointer to head of stack
+ * @line_number: line number of current operation
+ *
+ * Return: void
  */
 void pius_pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *temp;
+	stack_t *pop = *stack;
 
-	if (*stack == NULL)
+	if (var.stack_len == 0)
 	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		dprintf(STDOUT_FILENO,
+			"L%u: can't pop an empty stack\n",
+			line_number);
 		exit(EXIT_FAILURE);
 	}
-
-	temp = *stack;
-	*stack = (*stack)->next;
-	if (*stack != NULL)
-		(*stack)->prev = NULL;
-	free(temp);
+	(*stack)->next->prev = (*stack)->prev;
+	(*stack)->prev->next = (*stack)->next;
+	if (var.stack_len != 1)
+		*stack = (*stack)->next;
+	else
+		*stack = NULL;
+	free(pop);
+	var.stack_len--;
 }
