6f4dada089c1491f59b9b4659ed860f56343c892
meron6 <khozameron5@gmail.com>
2024-01-18 08:17:55 -0800

    swap.c

--- swap.c
+++ swap.c
@@ -1,23 +1,21 @@
 #include "monty.h"
 
 /**
- * swap - swaps the top two elements of the stack
- * @stack: the stack
- * @line_number: current line number of the bytecode file
- *
- * Return: Nothing
- */
+  * swap - swap two elements of the list
+  * @stack: pointer to head of list
+  * @line_number: line number
+  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-    int temp = 0;
+	int temp;
 
-    if (*stack == NULL || (*stack)->next == NULL)
-    {
-        dprintf(2, "L%d: can't swap, stack too short\n", line_number);
-        exit(EXIT_FAILURE);
-    }
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
 
-    temp = (*stack)->n;
-    (*stack)->n = (*stack)->next->n;
-    (*stack)->next->n = temp;
 }
