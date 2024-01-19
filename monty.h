21f93c1c2e10c7745c96f88e04603f182f53e066
meron6 <khozameron5@gmail.com>
2024-01-19 14:11:54 -0800

    update

--- monty.h
+++ monty.h
@@ -1,60 +1,32 @@
 #ifndef MONTY_H
 #define MONTY_H
 
-/* STRUCTS */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
+ * struct stack_s - doubly linked list representation of a stack
  * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * @prev: points to the previous element of the stack
+ * @next: points to the next element of the stack
  *
- * Description: Doubly linked list node structure for stack, queues, LIFO, FIFO
+ * Description: doubly linked list node structure for stack
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+    int n;
+    struct stack_s *prev;
+    struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: Opcode and its function for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
-/* PROTOTYPES */
-/**
- * Meron_pius1 - Print all values on `stack' starting from the top.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number being executed from the script file (currently unused).
- *
- * Return: void
- */
-void Meron_pius1(stack_t **stack, unsigned int line_number);
-/**
- * m_pint - Placeholder function, replace with actual implementation.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number being executed from the script file (currently unused).
- *
- * Return: void
- */
-void m_pint(stack_t **stack, unsigned int line_number);
-
-/**
- * monty_pint - Placeholder function, replace with actual implementation.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number being executed from the script file (currently unused).
- *
- * Return: void
- */
-void monty_pint(stack_t **stack, unsigned int line_number);
+/* Function prototypes */
+void push(stack_t **stack, int value);
+void pall(stack_t *stack);
+void free_stack(stack_t **stack);
+void process_file(char *filename, stack_t **stack);
+void process_line(char *line, stack_t **stack, unsigned int line_number);
+int is_integer(char *str);
 
 #endif /* MONTY_H */
