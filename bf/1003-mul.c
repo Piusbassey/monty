b94e5c8431534b26a63f926c9ac30f95d6a4029c
meron6 <khozameron5@gmail.com>
2024-01-20 11:15:12 -0800

    1003-mul.c

--- bf/1003-mul.c
+++ bf/1003-mul.c
@@ -1,1 +1,15 @@
-,>++++++[<-------->-],[<+>-]<.
+#include <stdio.h>
+
+int main()
+{
+    int a, b, result;
+
+    printf("Enter two digits: ");
+    scanf("%d %d", &a, &b);
+
+    result = a * b;
+
+    printf("The result is: %d\n", result);
+
+    return 0;
+}
