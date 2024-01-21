7a02000e6365d2f9086219818edc1225d8b80d2f
Piusbassey <okonpius5@gmail.com>
2024-01-21 15:40:55 +0000

    meron2_pius_add

--- meron2_pius_add.c
+++ meron2_pius_add.c
@@ -1,8 +1,16 @@
 #include "monty.h"
 
 /**
+ * add_data_mp - Adds the two elements of the stack from top.
+ * @stack: Double pointer to the head of the stack.
+ * @line_number: Line number being executed from the script.
  *
  */
-void add_data_mp(stack_t **stack, unsigned int boundary)
+void sum_data_mp(stack_t **stack, unsigned int boundary)
 {
-	int 
+	int res_ep;
+	stack_t *tpr = *stack;
+
+	if (len_ep(*stack) < 2)
+	{
+		fprint(stderr, "L%d: stack is not enough\n",
